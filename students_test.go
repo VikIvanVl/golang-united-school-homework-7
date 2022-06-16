a95a1e8a424f0ddbc0a544077d8150dfe5daf795
Ivan Victorov <victorov.i.vl@yandex.ru>
2022-06-16 07:57:16 +0300

    Init project

--- students_test.go
+++ students_test.go
@@ -1,7 +1,12 @@
 package coverage
 
 import (
+	"errors"
 	"os"
+	"reflect"
+	"strconv"
+	"testing"
+	"time"
 )
 
 // DO NOT EDIT THIS FUNCTION
@@ -18,3 +23,88 @@ func init() {
 
 // WRITE YOUR CODE BELOW
 
+func TestLess(t *testing.T) {
+	var people People
+
+	people = append(people, Person{"1", "1", time.Time{}})
+	people = append(people, Person{"1", "1", time.Time{}.Add(5 * time.Minute)})
+	people = append(people, Person{"2", "1", time.Time{}})
+	people = append(people, Person{"2", "2", time.Time{}})
+	people = append(people, Person{"2", "2", time.Time{}})
+
+	if people.Less(0, 1) {
+		t.Errorf("Error. Less by Birthday")
+	}
+
+	if people.Less(0, 2) {
+		t.Errorf("Error. Less by FirstName")
+	}
+
+	if people.Less(2, 3) {
+		t.Errorf("Error. Less by LastName")
+	}
+
+	if people.Less(3, 4) {
+		t.Errorf("Error. Less Eq")
+	}
+}
+
+func TestSwap(t *testing.T) {
+	var people People
+
+	peopleOne := Person{"1", "1", time.Time{}}
+	peopleTwo := Person{"1", "1", time.Time{}}
+
+	people = append(people, peopleOne)
+	people = append(people, peopleTwo)
+
+	people.Swap(0, 1)
+
+	if people[0] != peopleTwo || people[1] != peopleOne {
+		t.Errorf("Error. Swap")
+	}
+}
+
+func TestNew(t *testing.T) {
+	actual, err := New("---")
+	if actual != nil || !errors.Is(err, strconv.ErrSyntax) {
+		t.Errorf("Error. String Error")
+	}
+
+	actual, err = New("1 1 \n 2")
+	if actual != nil || err.Error() != "Rows need to be the same length" {
+		t.Errorf("Error. Matrix Error")
+	}
+
+	actual, err = New("1 1 \n 2 3")
+	expects := &Matrix{2, 2, []int{1, 1, 2, 3}}
+
+	if actual.cols != expects.cols || actual.rows != expects.rows || !reflect.DeepEqual(actual.data, expects.data) {
+		t.Errorf("Error. Empty Matrix")
+	}
+}
+
+func TestRows(t *testing.T) {
+	matrix := &Matrix{2, 2, []int{4, 5, 6, 7}}
+	expects := [][]int{{4, 5}, {6, 7}}
+
+	actual := matrix.Rows()
+
+	if !reflect.DeepEqual(actual, expects) {
+		t.Errorf("Error. Rows Matrix")
+	}
+}
+
+func TestCols(t *testing.T) {
+	matrix := &Matrix{3, 3, []int{1, 10, 100, 2, 20, 200, 3, 30, 300}}
+	expects := [][]int{{1, 2, 3}, {10, 20, 30}, {100, 200, 300}}
+
+	actual := matrix.Cols()
+
+	if !reflect.DeepEqual(actual, expects) {
+		t.Errorf("Error. Cols Matrix")
+	}
+}
+
+func TestSet(t *testing.T) {
+}
